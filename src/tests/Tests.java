bf28848717009a0f0c4c9a6691e4afa63260514c
SonVu <>
2022-08-22 09:37:00 +0700

    Updated Tests class.

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -7,7 +7,10 @@ import utils.NameGenerator;
 public class Tests {
     @Test
     public void Test1() {
-        String SonVu = NameGenerator.generateRandomName(12);
+        int stringLength = 5; // Number of characters of SonVu is 5.
+        String SonVu = NameGenerator.generateRandomName(stringLength);
+//        System.out.println(SonVu);
+        Assert.assertEquals(SonVu.length(), stringLength);
     }
 
 }
